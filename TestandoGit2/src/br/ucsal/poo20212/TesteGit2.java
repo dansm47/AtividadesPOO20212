6bc0e4b1340a68a12b122d7583781d0ea6ac651f
dansm <dansm@DESKTOP-7U3HI43>
2021-08-18 22:43:28 -0300

    Teste comit 2

---
+++ TestandoGit2/src/br/ucsal/poo20212/TesteGit2.java
@@ -0,0 +1,20 @@
+package br.ucsal.poo20212;
+
+import java.util.Scanner;
+
+public class TesteGit2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite o primeiro numero");
+		int num1 = sc.nextInt();
+		System.out.println("Digite o segundo numero");
+		int num2 = sc.nextInt();
+		int result = num1+num2;
+		System.out.println("Resultado da soma é " +result);
+		sc.close();
+
+	}
+
+}
